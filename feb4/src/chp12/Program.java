6d30bcb36aa58a9d56b4f559a4075d65c6332b83
Lucy Yu <37672833+lucyyu540@users.noreply.github.com>
2021-02-05 10:32:24 +0900

    Add files via upload

--- feb4/src/chp12/Program.java
+++ feb4/src/chp12/Program.java
@@ -1,26 +1,15 @@
-package chp12;
+package ch12;
 import java.util.*;
 public class Program {
 	private Set<Member> members = new TreeSet<Member>();
-	/*1*/public void signUp(String name, int id) { 
-		for (Member m : this.members) {
-			if(m.getID()==id) {
-				System.out.println("id already exists");
-				return;
-			}
-		}
-		members.add(new Member(name, id));
-		System.out.println("sign up succesful");
+	/*1*/public void signUp(String name, int id) {
+		if(members.add(new Member(name, id))) System.out.println("sign up succesful");
+		else System.out.println("id already exists");
 	}
-	/*2*/public void deleteMember(String name, int id) {
-		for (Member m : members) {
-			if(m.getName().equals(name) && m.getID()==id) {
-				members.remove(m);
-				System.out.println("succesfully deleted");
-				return;
-			}
-		}
-		System.out.println("member not found");
+	/*2*/public void deleteMember(int id) {
+		Member temp = new Member("", id);
+		if(members.remove(temp)) System.out.println("succesfully deleted");
+		else System.out.println("member not found");
 	}
 	/*3*/public void print() { for(Member mem : this.members) {mem.print();} }
 	public void menu(Scanner in) {
@@ -28,13 +17,17 @@ public class Program {
 		System.out.println("1) sign up\n2) delete member\n3) print all\n4) exit");
 		System.out.println("***********");
 		int menu = in.nextInt();
-		if(menu == 1 || menu == 2) {
+		if(menu == 1) {
 			System.out.print("name: ");
 			String name = in.next();
 			System.out.print("id: ");
 			int id = in.nextInt();
-			if(menu == 1) this.signUp(name,id);
-			else this.deleteMember(name,id);
+			this.signUp(name,id);
+		}
+		else if(menu == 2) {
+			System.out.print("id: ");
+			int id = in.nextInt();
+			this.deleteMember(id);
 		}
 		else if (menu == 3) this.print();
 		else if (menu ==4) System.exit(0);
@@ -44,5 +37,22 @@ public class Program {
 		Program program = new Program();
 		Scanner in = new Scanner(System.in);
 		while(true) {program.menu(in);}
+	}
+}
+class Member implements Comparable<Member>{
+	private String name;
+	private int id; 
+	public Member(String s, int n) {
+		this.name = s; 
+		this.id = n;
+	}
+	public void print() {System.out.println("name: "+ this.name+ ", id: "+this.id);}
+	public String getName() {return this.name;}
+	public int getID() {return this.id;}
+	@Override
+	public int compareTo(Member o) { return this.id - o.getID(); }//acsending order
+	public boolean equals(Member o) {
+		if(this.id == o.getID()) return true;
+		else return false;
 	}
 }
